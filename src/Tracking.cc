7ada9b58efd8f586a16542bd55f7fe8607c9e6d1
kpbs5 <kristoffer.pbs@tuta.io>
2023-06-19 14:11:11 +0200

    refactor: change if statement logic to return early, and avoid needless scope

--- src/Tracking.cc
+++ src/Tracking.cc
@@ -2384,119 +2384,122 @@ void Tracking::Track()
 
 void Tracking::StereoInitialization()
 {
-    if(mCurrentFrame.N>500)
-    {
-        if (mSensor == System::IMU_STEREO || mSensor == System::IMU_RGBD)
-        {
-            if (!mCurrentFrame.mpImuPreintegrated || !mLastFrame.mpImuPreintegrated)
-            {
-                DEBUG_LOG(stderr, "IMU preintegration not set!");
-                // cout << "not IMU meas" << endl;
-                return;
-            }
-
-            const double imu_avg_acceleration =  (mCurrentFrame.mpImuPreintegratedFrame->avgA - mLastFrame.mpImuPreintegratedFrame->avgA).norm();
-            const double min_acceleration_required = 0.5;
-            const bool enough_acceleration = imu_avg_acceleration >= min_acceleration_required;
-            // if (!mFastInit && (mCurrentFrame.mpImuPreintegratedFrame->avgA-mLastFrame.mpImuPreintegratedFrame->avgA).norm()<0.5)
-            if (!mFastInit && !enough_acceleration)
-            {
-                DEBUG_LOG(stderr, "not enough acceleration. %f < %f", imu_avg_acceleration, min_acceleration_required);
-                return;
-            }
-
-            if(mpImuPreintegratedFromLastKF) {
-                delete mpImuPreintegratedFromLastKF;
-            }
-            mpImuPreintegratedFromLastKF = new IMU::Preintegrated(IMU::Bias(),*mpImuCalib);
-            mCurrentFrame.mpImuPreintegrated = mpImuPreintegratedFromLastKF;
-        }
-
-        // Set Frame pose to the origin (In case of inertial SLAM to imu)
-        if (mSensor == System::IMU_STEREO || mSensor == System::IMU_RGBD)
-        {
-            Eigen::Matrix3f Rwb0 = mCurrentFrame.mImuCalib.mTcb.rotationMatrix();
-            Eigen::Vector3f twb0 = mCurrentFrame.mImuCalib.mTcb.translation();
-            Eigen::Vector3f Vwb0;
-            Vwb0.setZero();
-            mCurrentFrame.SetImuPoseVelocity(Rwb0, twb0, Vwb0);
-        }
-        else {
-            mCurrentFrame.SetPose(Sophus::SE3f());
-        }
-        // Create KeyFrame
-        KeyFrame* pKFini = new KeyFrame(mCurrentFrame,mpAtlas->GetCurrentMap(),mpKeyFrameDB);
-
-        // Insert KeyFrame in the map
-        mpAtlas->AddKeyFrame(pKFini);
-
-        // Create MapPoints and asscoiate to KeyFrame
-        if(!mpCamera2){
-            for(int i=0; i<mCurrentFrame.N;i++)
-            {
-                float z = mCurrentFrame.mvDepth[i];
-                if(z>0)
-                {
-                    Eigen::Vector3f x3D;
-                    mCurrentFrame.UnprojectStereo(i, x3D);
-                    MapPoint* pNewMP = new MapPoint(x3D, pKFini, mpAtlas->GetCurrentMap());
-                    pNewMP->AddObservation(pKFini,i);
-                    pKFini->AddMapPoint(pNewMP,i);
-                    pNewMP->ComputeDistinctiveDescriptors();
-                    pNewMP->UpdateNormalAndDepth();
-                    mpAtlas->AddMapPoint(pNewMP);
-
-                    mCurrentFrame.mvpMapPoints[i]=pNewMP;
-                }
-            }
-        } else{
-            for(int i = 0; i < mCurrentFrame.Nleft; i++){
-                int rightIndex = mCurrentFrame.mvLeftToRightMatch[i];
-                if(rightIndex != -1){
-                    Eigen::Vector3f x3D = mCurrentFrame.mvStereo3Dpoints[i];
-
-                    MapPoint* pNewMP = new MapPoint(x3D, pKFini, mpAtlas->GetCurrentMap());
-
-                    pNewMP->AddObservation(pKFini,i);
-                    pNewMP->AddObservation(pKFini,rightIndex + mCurrentFrame.Nleft);
-
-                    pKFini->AddMapPoint(pNewMP,i);
-                    pKFini->AddMapPoint(pNewMP,rightIndex + mCurrentFrame.Nleft);
-
-                    pNewMP->ComputeDistinctiveDescriptors();
-                    pNewMP->UpdateNormalAndDepth();
-                    mpAtlas->AddMapPoint(pNewMP);
-
-                    mCurrentFrame.mvpMapPoints[i]=pNewMP;
-                    mCurrentFrame.mvpMapPoints[rightIndex + mCurrentFrame.Nleft]=pNewMP;
-                }
-            }
-        }
-
-        // Verbose::PrintMess("New Map created with " + to_string(mpAtlas->MapPointsInMap()) + " points", Verbose::VERBOSITY_QUIET);
-        DEBUG_LOG(stderr, "New Map created with %ld points", mpAtlas->MapPointsInMap());
-        //cout << "Active map: " << mpAtlas->GetCurrentMap()->GetId() << endl;
-
-        mpLocalMapper->InsertKeyFrame(pKFini);
-
-        mLastFrame = Frame(mCurrentFrame);
-        mnLastKeyFrameId = mCurrentFrame.mnId;
-        mpLastKeyFrame = pKFini;
-        //mnLastRelocFrameId = mCurrentFrame.mnId;
-
-        mvpLocalKeyFrames.push_back(pKFini);
-        mvpLocalMapPoints=mpAtlas->GetAllMapPoints();
-        mpReferenceKF = pKFini;
-        mCurrentFrame.mpReferenceKF = pKFini;
-
-        mpAtlas->SetReferenceMapPoints(mvpLocalMapPoints);
-
-        mpAtlas->GetCurrentMap()->mvpKeyFrameOrigins.push_back(pKFini);
-
-        mpMapDrawer->SetCurrentCameraPose(mCurrentFrame.GetPose());
-
-        mState=OK;
+    // QUESTION(19-06-2023 14:01:41, kristoffer, magic-constant): Why 500?
+    if (mCurrentFrame.N <= 500) {
+        return;
     }
+
+    if (mSensor == System::IMU_STEREO || mSensor == System::IMU_RGBD)
+    {
+        if (!mCurrentFrame.mpImuPreintegrated || !mLastFrame.mpImuPreintegrated)
+        {
+            DEBUG_LOG(stderr, "IMU preintegration not set!");
+            // cout << "not IMU meas" << endl;
+            return;
+        }
+
+        const double imu_avg_acceleration =  (mCurrentFrame.mpImuPreintegratedFrame->avgA - mLastFrame.mpImuPreintegratedFrame->avgA).norm();
+        const double min_acceleration_required = 0.5;
+        const bool enough_acceleration = imu_avg_acceleration >= min_acceleration_required;
+        // if (!mFastInit && (mCurrentFrame.mpImuPreintegratedFrame->avgA-mLastFrame.mpImuPreintegratedFrame->avgA).norm()<0.5)
+        if (!mFastInit && !enough_acceleration)
+        {
+            DEBUG_LOG(stderr, "not enough acceleration. %f < %f", imu_avg_acceleration, min_acceleration_required);
+            return;
+        }
+
+        if(mpImuPreintegratedFromLastKF) {
+            delete mpImuPreintegratedFromLastKF;
+        }
+        mpImuPreintegratedFromLastKF = new IMU::Preintegrated(IMU::Bias(),*mpImuCalib);
+        mCurrentFrame.mpImuPreintegrated = mpImuPreintegratedFromLastKF;
+    }
+
+    // Set Frame pose to the origin (In case of inertial SLAM to imu)
+    if (mSensor == System::IMU_STEREO || mSensor == System::IMU_RGBD)
+    {
+        Eigen::Matrix3f Rwb0 = mCurrentFrame.mImuCalib.mTcb.rotationMatrix();
+        Eigen::Vector3f twb0 = mCurrentFrame.mImuCalib.mTcb.translation();
+        Eigen::Vector3f Vwb0;
+        Vwb0.setZero();
+        mCurrentFrame.SetImuPoseVelocity(Rwb0, twb0, Vwb0);
+    }
+    else {
+        mCurrentFrame.SetPose(Sophus::SE3f());
+    }
+    // Create KeyFrame
+    KeyFrame* pKFini = new KeyFrame(mCurrentFrame,mpAtlas->GetCurrentMap(),mpKeyFrameDB);
+
+    // Insert KeyFrame in the map
+    mpAtlas->AddKeyFrame(pKFini);
+
+    // Create MapPoints and asscoiate to KeyFrame
+    if(!mpCamera2){
+        for(int i=0; i<mCurrentFrame.N;i++)
+        {
+            float z = mCurrentFrame.mvDepth[i];
+            if(z>0)
+            {
+                Eigen::Vector3f x3D;
+                mCurrentFrame.UnprojectStereo(i, x3D);
+                MapPoint* pNewMP = new MapPoint(x3D, pKFini, mpAtlas->GetCurrentMap());
+                pNewMP->AddObservation(pKFini,i);
+                pKFini->AddMapPoint(pNewMP,i);
+                pNewMP->ComputeDistinctiveDescriptors();
+                pNewMP->UpdateNormalAndDepth();
+                mpAtlas->AddMapPoint(pNewMP);
+
+                mCurrentFrame.mvpMapPoints[i]=pNewMP;
+            }
+        }
+    } else{
+        for(int i = 0; i < mCurrentFrame.Nleft; i++){
+            int rightIndex = mCurrentFrame.mvLeftToRightMatch[i];
+            if(rightIndex != -1){
+                Eigen::Vector3f x3D = mCurrentFrame.mvStereo3Dpoints[i];
+
+                MapPoint* pNewMP = new MapPoint(x3D, pKFini, mpAtlas->GetCurrentMap());
+
+                pNewMP->AddObservation(pKFini,i);
+                pNewMP->AddObservation(pKFini,rightIndex + mCurrentFrame.Nleft);
+
+                pKFini->AddMapPoint(pNewMP,i);
+                pKFini->AddMapPoint(pNewMP,rightIndex + mCurrentFrame.Nleft);
+
+                pNewMP->ComputeDistinctiveDescriptors();
+                pNewMP->UpdateNormalAndDepth();
+                mpAtlas->AddMapPoint(pNewMP);
+
+                mCurrentFrame.mvpMapPoints[i]=pNewMP;
+                mCurrentFrame.mvpMapPoints[rightIndex + mCurrentFrame.Nleft]=pNewMP;
+            }
+        }
+    }
+
+    // Verbose::PrintMess("New Map created with " + to_string(mpAtlas->MapPointsInMap()) + " points", Verbose::VERBOSITY_QUIET);
+    DEBUG_LOG(stderr, "New Map created with %ld points", mpAtlas->MapPointsInMap());
+    //cout << "Active map: " << mpAtlas->GetCurrentMap()->GetId() << endl;
+
+    mpLocalMapper->InsertKeyFrame(pKFini);
+
+    mLastFrame = Frame(mCurrentFrame);
+    mnLastKeyFrameId = mCurrentFrame.mnId;
+    mpLastKeyFrame = pKFini;
+    //mnLastRelocFrameId = mCurrentFrame.mnId;
+
+    mvpLocalKeyFrames.push_back(pKFini);
+    mvpLocalMapPoints=mpAtlas->GetAllMapPoints();
+    mpReferenceKF = pKFini;
+    mCurrentFrame.mpReferenceKF = pKFini;
+
+    mpAtlas->SetReferenceMapPoints(mvpLocalMapPoints);
+
+    mpAtlas->GetCurrentMap()->mvpKeyFrameOrigins.push_back(pKFini);
+
+    mpMapDrawer->SetCurrentCameraPose(mCurrentFrame.GetPose());
+
+    mState=OK;
+
 }
 
 
